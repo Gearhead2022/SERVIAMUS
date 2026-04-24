b809ad51af46904f539257dd1a78b5f4fb936ec9
John3888 <johndanielflores3@gmail.com>
2026-04-24 08:21:34 +0800

    merged main

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
 
+<<<<<<< HEAD
     allowedDevOrigins: [
         "172.168.30.107",
         "172.168.30.107:5006",
@@ -9,6 +10,12 @@ const nextConfig: NextConfig = {
         "192.168.1.143",
         "192.168.1.143:3006",
         "192.168.1.143:5006"
+=======
+    allowedDevOrigins:[
+    "192.168.1.9",
+        "192.168.1.9:5006",
+        "192.168.1.9:3006",
+>>>>>>> 926af12 (reseted)
     ]
 
 };
