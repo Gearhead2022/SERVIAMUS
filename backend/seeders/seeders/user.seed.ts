67c989a646d232c1836ec11dc08ec923c556e6a0
John3888 <johndanielflores3@gmail.com>
2026-08-24 15:51:04 +0800

    added user seeds

--- backend/seeders/seeders/user.seed.ts
+++ backend/seeders/seeders/user.seed.ts
@@ -69,6 +69,40 @@ export async function seedUsers(prisma: PrismaClient) {
         },
     });
 
+    const pathologist1 = await prisma.users.create({
+        data: {
+            name: "DR. GREG RYAN T. GERONGANO",
+            username: "dr_gerongano",
+            password: await hash("dr_gerongano"),
+            title: "MD. , DPSP",
+            ptr_no: "",
+            license_no: "0128041"
+        },
+    });
+
+    const medtech1 = await prisma.users.create({
+        data: {
+            name: "MA. ESTELA N. ISUBAL",
+            username: "medtech_isubal",
+            password: await hash("medtech_isubal"),
+            title: "RMT",
+            ptr_no: "",
+            license_no: "0041101"
+        },
+    });
+
+    const medtech2 = await prisma.users.create({
+        data: {
+            name: "JHENNY S. ALVAREZ",
+            username: "medtech_alvarez",
+            password: await hash("medtech_alvarez"),
+            title: "RMT",
+            ptr_no: "",
+            license_no: "00112550"
+        },
+    });
+
+
 
     await prisma.userRole.createMany({
         data: [
@@ -96,8 +130,20 @@ export async function seedUsers(prisma: PrismaClient) {
                 user_id: encoder1.user_id,
                 role_id: roleMap["ENCODER"],
             },
+            {
+                user_id: pathologist1.user_id,
+                role_id: roleMap["PATHOLOGIST"],
+            },
+            {
+                user_id: medtech1.user_id,
+                role_id: roleMap["LAB"],
+            },
+            {
+                user_id: medtech2.user_id,
+                role_id: roleMap["LAB"],
+            },
         ]
     });
 
-    return { doctor1, staff1, admin1, cashier1, lab1, encoder1 };
+    return { doctor1, staff1, admin1, cashier1, lab1, encoder1, pathologist1, medtech1, medtech2 };
 }
